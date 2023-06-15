4309e8403d7563d2967fc77a7536981fbfab722d
salman4hussain <salman4hussain@gmail.com>
2023-06-16 00:28:53 +0530

    Added initial commit

---
+++ src/main/java/com/thinkify/interview/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.thinkify.interview.controller;
+
+import com.thinkify.interview.entity.User;
+import com.thinkify.interview.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerSubmit(@ModelAttribute User user) {
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
+
---
+++ src/main/java/com/thinkify/interview/entity/User.java
@@ -0,0 +1,50 @@
+package com.thinkify.interview.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    // Getters and setters
+
+    // Constructors
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
